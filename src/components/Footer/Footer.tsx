7cdf4725220377765083833bbdb530e62e7211c5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 17:01:29 -0500

    added logo to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,24 @@
-import "./scss/index.scss";
+import './scss/index.scss';
 
-import * as React from "react";
+import * as React from 'react';
 
-import { SocialMediaIcon } from "..";
-import { SOCIAL_MEDIA } from "../../core/config";
-import Nav from "./Nav";
+import ReactSVG from 'react-svg';
+import { SocialMediaIcon } from '..';
+import { SOCIAL_MEDIA } from '../../core/config';
+import Nav from './Nav';
 
+import logo from '../../images/logo/icon-with-typeface.svg';
 
 const Footer: React.FC = () => (
-  <div className="footer" id="footer">
-    <div className="footer__favicons container">
-      {SOCIAL_MEDIA.map(medium => (
-        <SocialMediaIcon medium={medium} key={medium.ariaLabel} />
-      ))}
+    <div className="footer" id="footer">
+        <a href="/"><ReactSVG path={logo} className="footer__logo"/></a>
+        <div className="footer__favicons container">
+            {SOCIAL_MEDIA.map(medium => (
+                <SocialMediaIcon medium={medium} key={medium.ariaLabel}/>
+            ))}
+        </div>
+        <Nav/>
     </div>
-    <Nav />
-  </div>
 );
 
 export default Footer;
