ed6a0f6233344765928a1e44567fb31499e496d0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-18 18:44:22 -0500

    corrected address book plus color

--- src/@next/components/atoms/AddNewTile/AddNewTile.tsx
+++ src/@next/components/atoms/AddNewTile/AddNewTile.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import { Icon } from "../Icon";
-import { Tile } from "../Tile";
-import * as S from "./styles";
-import { IProps } from "./types";
+import { sadboysPink } from '@styles/constants';
+import { Icon } from '../Icon';
+import { Tile } from '../Tile';
+import * as S from './styles';
+import { IProps } from './types';
 
-export const AddNewTile: React.FC<IProps> = ({ type, ...props }: IProps) => {
-  return (
-    <Tile tileType="addNew" {...props}>
-      <S.Content>
-        <p>
-          <Icon size={24} name="plus" />
-        </p>
-        <p>Add new {type}</p>
-      </S.Content>
-    </Tile>
-  );
+export const AddNewTile: React.FC<IProps> = ({type, ...props}: IProps) => {
+    return (
+        <Tile tileType="addNew" {...props}>
+            <S.Content>
+                <p>
+                    <Icon size={24} name="plus" color={sadboysPink}/>
+                </p>
+                <p>Add new {type}</p>
+            </S.Content>
+        </Tile>
+    );
 };
