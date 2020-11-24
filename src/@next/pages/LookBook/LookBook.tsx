da6663285a34a37cb3dbb581085c92abda4501d9
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 15:13:55 -0500

    built lookbook page

---
+++ src/@next/pages/LookBook/LookBook.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './index.scss';
+
+const LookBook: React.FC = () => {
+    return (
+        <div className="lookbook-wrapper">
+            <img src="https://via.placeholder.com/1920x1080?text=Lookbook+Image" alt="lookbook 1"/>
+            <img src="https://via.placeholder.com/1920x1080?text=Lookbook+Image" alt="lookbook 2"/>
+            <img src="https://via.placeholder.com/1920x1080?text=Lookbook+Image" alt="lookbook 3"/>
+            <img src="https://via.placeholder.com/1920x1080?text=Lookbook+Image" alt="lookbook 4"/>
+            <img src="https://via.placeholder.com/1920x1080?text=Lookbook+Image" alt="lookbook 5"/>
+            <img src="https://via.placeholder.com/1920x1080?text=Lookbook+Image" alt="lookbook 6"/>
+        </div>
+    )
+};
+
+export { LookBook };
